2930aafae83ce244e2de4ca2234134fd7fd6a37a
devnewton <devnewton@bci.im>
2020-11-12 15:52:38 +0100

    use external tilesets

---
+++ public_html/maps/mountain/collectibles.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="collectibles" tilewidth="32" tileheight="32" tilecount="64" columns="8">
+ <image source="../../sprites/collectibles.png" width="256" height="256"/>
+ <tile id="0">
+  <properties>
+   <property name="type" value="heart"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="type" value="key"/>
+  </properties>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="type" value="exit"/>
+  </properties>
+ </tile>
+</tileset>
