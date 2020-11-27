9849c190a5e11c5565495e0068e65f43c8053166
devnewton <devnewton@bci.im>
2020-11-27 17:49:01 +0100

    tower demon

--- public_html/maps/tower/tower.tsx
+++ public_html/maps/tower/tower.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.1" name="mountain" tilewidth="32" tileheight="32" spacing="2" margin="1" tilecount="49" columns="7">
+<tileset version="1.4" tiledversion="1.4.2" name="tower" tilewidth="32" tileheight="32" spacing="2" margin="1" tilecount="49" columns="7">
  <image source="tower.png" width="256" height="256"/>
 </tileset>
