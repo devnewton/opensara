7b3d1ccebd85991169828e36a601918b5b64a25c
devnewton <devnewton@bci.im>
2020-11-27 18:08:04 +0100

    fix tower bg

--- public_html/maps/tower/tower_bg.tsx
+++ public_html/maps/tower/tower_bg.tsx
@@ -1,2 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="tower_bg" tilewidth="1" tileheight="1" tilecount="0" columns="0"/>
+<tileset version="1.4" tiledversion="1.4.2" name="tower_bg" tilewidth="256" tileheight="256" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="256" height="256" source="demon_tile.png"/>
+ </tile>
+</tileset>
