b25d3dabecb2857eec14df20ee22525bc2d298cb
devnewton <devnewton@bci.im>
2020-11-30 18:31:04 +0100

    tower bg0 tileset

---
+++ public_html/maps/tower/tower_bg0_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="tower_bg0_tileset" tilewidth="256" tileheight="256" spacing="2" margin="2" tilecount="9" columns="3">
+ <image source="tower_bg0_tileset.png" width="1024" height="1024"/>
+</tileset>
