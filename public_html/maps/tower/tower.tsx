f25b3c5e2a388c6f6cd84e6c8506d487e9062267
devnewton <devnewton@bci.im>
2020-12-02 14:20:44 +0100

    lava animation

--- public_html/maps/tower/tower.tsx
+++ public_html/maps/tower/tower.tsx
@@ -1,4 +1,28 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.2" name="tower" tilewidth="32" tileheight="32" spacing="2" margin="1" tilecount="49" columns="7">
  <image source="tower.png" width="256" height="256"/>
+ <tile id="44">
+  <animation>
+   <frame tileid="28" duration="100"/>
+   <frame tileid="29" duration="100"/>
+   <frame tileid="30" duration="100"/>
+   <frame tileid="35" duration="100"/>
+   <frame tileid="36" duration="100"/>
+   <frame tileid="37" duration="100"/>
+   <frame tileid="42" duration="100"/>
+   <frame tileid="43" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="47">
+  <animation>
+   <frame tileid="31" duration="100"/>
+   <frame tileid="32" duration="100"/>
+   <frame tileid="33" duration="100"/>
+   <frame tileid="38" duration="100"/>
+   <frame tileid="39" duration="100"/>
+   <frame tileid="40" duration="100"/>
+   <frame tileid="45" duration="100"/>
+   <frame tileid="46" duration="100"/>
+  </animation>
+ </tile>
 </tileset>
