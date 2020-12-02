9c09c8c426f0627a190dda3638e3202e4db59d6d
devnewton <devnewton@bci.im>
2020-12-02 19:27:41 +0100

    lava deep tile

--- public_html/maps/tower/tower_bg.tsx
+++ public_html/maps/tower/tower_bg.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="tower_bg" tilewidth="1024" tileheight="1024" tilecount="5" columns="0">
+<tileset version="1.4" tiledversion="1.4.2" name="tower_bg" tilewidth="1024" tileheight="1024" tilecount="6" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="256" height="256" source="tower_bg0_brick1.png"/>
@@ -15,5 +15,8 @@
  </tile>
  <tile id="4">
   <image width="1024" height="1024" source="sky.png"/>
+ </tile>
+ <tile id="5">
+  <image width="256" height="256" source="lava_deep.png"/>
  </tile>
 </tileset>
