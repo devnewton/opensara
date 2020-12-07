ac5e9d3c72a37c31d4b80d154a96601b5156c2ac
devnewton <devnewton@bci.im>
2020-12-07 19:29:54 +0100

    better sky and brick

--- public_html/maps/tower/tower_bg.tsx
+++ public_html/maps/tower/tower_bg.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="tower_bg" tilewidth="1024" tileheight="1024" tilecount="6" columns="0">
+<tileset version="1.4" tiledversion="1.4.2" name="tower_bg" tilewidth="1024" tileheight="1024" tilecount="7" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="256" height="256" source="tower_bg0_brick1.png"/>
@@ -18,5 +18,8 @@
  </tile>
  <tile id="5">
   <image width="256" height="256" source="lava_deep.png"/>
+ </tile>
+ <tile id="6">
+  <image width="256" height="256" source="tower_bg0_brick3.png"/>
  </tile>
 </tileset>
