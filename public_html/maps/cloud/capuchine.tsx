3637d017b47a4c8ed9ffed32d4ab52b1fc5a258e
devnewton <devnewton@bci.im>
2021-06-23 10:40:44 +0200

    capuchine tileset

---
+++ public_html/maps/cloud/capuchine.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="capuchine" tilewidth="32" tileheight="32" tilecount="64" columns="8">
+ <image source="../../sprites/capuchine.png" width="260" height="260"/>
+</tileset>
